5a2d49837b5338e2a6ed126403f05b20c2c4ff75
tiemen <tiemenr@gmail.com>
2020-10-26 12:38:44 +0100

    simple prompt

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
+#include <stdio.h>
+#include "get_next_line.h"
+
 int main()
 {
-	printf("hoi");
-	printf("Tiemen");
+	int		status;
+	char	*line;
+
+	status = 1;
+	while (status)
+	{
+		write(1, "> ", 2);
+		get_next_line(0, &line);
+		printf("%s\n", line);
+	}
 	return (0);
 }
