636831704470a18121b479bde38b7929a6976bf0
gbouwen <gijsbouwen@gmail.com>
2020-10-27 17:07:14 +0100

    fixed merg conflicts

--- main.c
+++ main.c
@@ -6,11 +6,7 @@
 /*   By: tiemen <tiemen@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/26 12:50:24 by tiemen        #+#    #+#                 */
-<<<<<<< HEAD
-/*   Updated: 2020/10/27 15:43:39 by gbouwen       ########   odam.nl         */
-=======
-/*   Updated: 2020/10/27 16:40:37 by tiemen        ########   odam.nl         */
->>>>>>> 44a3eff8145a97637e2a26c5333144f6c0a0a5e5
+/*   Updated: 2020/10/27 17:06:13 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +22,9 @@ int	main(void)
 	status = 1;
 	while (status)
 	{
-<<<<<<< HEAD
 		ft_printf("> ");
 		get_next_line(0, &line);
 		ft_printf("%s\n", line);
-=======
 		get_next_line(0, &line);
 		lexer(&lexer_list, line, ft_strlen(line));
 		t_list *temp = lexer_list.token_list;
@@ -40,7 +34,6 @@ int	main(void)
 			temp = temp->next;
 		}
 		printf("%s\n", line);
->>>>>>> 44a3eff8145a97637e2a26c5333144f6c0a0a5e5
 	}
 	return (0);
 }
--- minishell.h
+++ minishell.h
@@ -6,11 +6,7 @@
 /*   By: gbouwen <gbouwen@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/26 14:10:37 by gbouwen       #+#    #+#                 */
-<<<<<<< HEAD
-/*   Updated: 2020/10/27 15:43:03 by gbouwen       ########   odam.nl         */
-=======
-/*   Updated: 2020/10/27 15:43:14 by tiemen        ########   odam.nl         */
->>>>>>> 44a3eff8145a97637e2a26c5333144f6c0a0a5e5
+/*   Updated: 2020/10/27 17:07:06 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +15,11 @@
 
 # include "libft/libft.h"
 # include "get_next_line/get_next_line.h"
-<<<<<<< HEAD
 # include "ft_printf/libftprintf.h"
-
 # include "builtins/builtins.h"
-=======
 # include "lexer/lexer.h"
 
 
 # include <stdio.h>
->>>>>>> 44a3eff8145a97637e2a26c5333144f6c0a0a5e5
-
-
 
 #endif
