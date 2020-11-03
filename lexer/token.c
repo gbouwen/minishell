baac1c4beda5f50bec553309eceeae485acf54fe
gbouwen <gijsbouwen@gmail.com>
2020-11-03 12:56:08 +0100

    protected some mallocs

--- lexer/token.c
+++ lexer/token.c
@@ -6,7 +6,7 @@
 /*   By: gbouwen <gbouwen@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/28 16:00:51 by gbouwen       #+#    #+#                 */
-/*   Updated: 2020/10/29 17:45:23 by gbouwen       ########   odam.nl         */
+/*   Updated: 2020/11/03 12:50:24 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 void	init_token(t_list *token, int length)
 {
 	token->content = malloc(length + 1);
+	if (!token->content)
+		exit_error("Malloc failed");
 	ft_bzero(token->content, length + 1);
 	token->current_char = 0;
 	token->type = 0;
@@ -35,6 +37,8 @@ void	set_special_token(t_lexer *lexer_data, t_list **token, int i)
 	if ((i + 1) < lexer_data->line_length)
 	{
 		(*token)->next = malloc(sizeof(t_list));
+		if (!(*token)->next)
+			exit_error("Malloc failed");
 		*token = (*token)->next;
 		init_token(*token, lexer_data->line_length - i);
 	}
