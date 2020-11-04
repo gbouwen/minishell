94d496a50d0b4904056dea8bbbd8b9934e205e6d
gbouwen <gijsbouwen@gmail.com>
2020-11-04 13:55:42 +0100

    fixed leaks in lexer

--- lexer/lexer.h
+++ lexer/lexer.h
@@ -6,7 +6,7 @@
 /*   By: tiemen <tiemen@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/27 15:26:26 by tiemen        #+#    #+#                 */
-/*   Updated: 2020/11/02 14:24:50 by gbouwen       ########   odam.nl         */
+/*   Updated: 2020/11/04 13:39:52 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,15 @@
 
 # include "../minishell.h"
 
-void	lexer(t_lexer *lexer_data, char *line, int length);
+void	lexer(t_lexer *lexer, char *line);
 
 int		get_char_type(char c);
 
-void	state_check(t_lexer *lexer_data, t_list **token, char *line, int i);
-void	state_general(t_lexer *lexer_data, t_list **token, char *line, int i);
-void	state_single_quote(t_lexer *lexer_data, t_list **token, char c);
-void	state_double_quote(t_lexer *lexer_data, t_list **token, char c);
+void	state_check(t_lexer *lexer, t_list **token, char *line, int i);
 
-void	init_token(t_list *token, int length);
+void	init_token(t_lexer *lexer, t_list *token, char *line, int length);
 void	set_token_data(t_list **token, char c);
-void	set_special_token(t_lexer *lexer_data, t_list **token, int i);
-void	end_token(t_lexer *lexer_data, t_list **token, int i);
+void	set_special_token(t_lexer *lexer, char *line, t_list **token, int i);
+void	end_token(t_lexer *lexer, char *line, t_list **token, int i);
 
 #endif
--- lexer/state.c
+++ lexer/state.c
@@ -6,23 +6,13 @@
 /*   By: gbouwen <gbouwen@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/28 16:05:34 by gbouwen       #+#    #+#                 */
-/*   Updated: 2020/11/02 16:23:49 by gbouwen       ########   odam.nl         */
+/*   Updated: 2020/11/04 13:37:57 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
 
-void	state_check(t_lexer *lexer_data, t_list **token, char *line, int i)
-{
-	if (lexer_data->state == GENERAL)
-		state_general(lexer_data, token, line, i);
-	else if (lexer_data->state == IN_QUOTE)
-		state_single_quote(lexer_data, token, line[i]);
-	else if (lexer_data->state == IN_DOUBLE_QUOTE)
-		state_double_quote(lexer_data, token, line[i]);
-}
-
-void	state_general(t_lexer *lexer_data, t_list **token, char *line, int i)
+static void	state_general(t_lexer *lexer_data, t_list **token, char *line, int i)
 {
 	if (lexer_data->char_type == CHAR_GENERAL)
 		set_token_data(token, line[i]);
@@ -33,15 +23,15 @@ void	state_general(t_lexer *lexer_data, t_list **token, char *line, int i)
 	else if (lexer_data->char_type == CHAR_DOUBLE_QUOTE)
 		lexer_data->state = IN_DOUBLE_QUOTE;
 	else if (lexer_data->char_type == CHAR_WHITESPACE)
-		end_token(lexer_data, token, i);
+		end_token(lexer_data, line, token, i);
 	else
 	{
-		end_token(lexer_data, token, i);
-		set_special_token(lexer_data, token, i);
+		end_token(lexer_data, line, token, i);
+		set_special_token(lexer_data, line, token, i);
 	}
 }
 
-void	state_single_quote(t_lexer *lexer_data, t_list **token, char c)
+static void	state_single_quote(t_lexer *lexer_data, t_list **token, char c)
 {
 	if (lexer_data->char_type == CHAR_QUOTE)
 	{
@@ -53,7 +43,7 @@ void	state_single_quote(t_lexer *lexer_data, t_list **token, char c)
 	(*token)->current_char++;
 }
 
-void	state_double_quote(t_lexer *lexer_data, t_list **token, char c)
+static void	state_double_quote(t_lexer *lexer_data, t_list **token, char c)
 {
 	if (lexer_data->char_type == CHAR_DOUBLE_QUOTE)
 	{
@@ -63,4 +53,14 @@ void	state_double_quote(t_lexer *lexer_data, t_list **token, char c)
 	}
 	(*token)->content[(*token)->current_char] = c;
 	(*token)->current_char++;
+}
+
+void		state_check(t_lexer *lexer_data, t_list **token, char *line, int i)
+{
+	if (lexer_data->state == GENERAL)
+		state_general(lexer_data, token, line, i);
+	else if (lexer_data->state == IN_QUOTE)
+		state_single_quote(lexer_data, token, line[i]);
+	else if (lexer_data->state == IN_DOUBLE_QUOTE)
+		state_double_quote(lexer_data, token, line[i]);
 }
