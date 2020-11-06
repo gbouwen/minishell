3d60ffacedabf24d555370d045884f77ee3d5ebc
gbouwen <gijsbouwen@gmail.com>
2020-11-06 12:30:24 +0100

    small fix export

--- builtins/export_no_arguments.c
+++ builtins/export_no_arguments.c
@@ -6,18 +6,18 @@
 /*   By: gbouwen <marvin@codam.nl>                    +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/05 15:38:42 by gbouwen       #+#    #+#                 */
-/*   Updated: 2020/11/05 16:00:55 by gbouwen       ########   odam.nl         */
+/*   Updated: 2020/11/06 12:28:41 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 
-static int		get_envp_len(char **envp)
+static int		get_env_len(char **env)
 {
 	int	i;
 
 	i = 0;
-	while (envp[i] != NULL)
+	while (env[i] != NULL)
 		i++;
 	return (i);
 }
@@ -34,19 +34,34 @@ static void	initialize_indexes_array(int *sorted_indexes, int len)
 	}
 }
 
-static void	print_sorted(char **envp, int sorted_indexes[], int len)
+static void	print_sorted(char **env, int sorted_indexes[], int len)
 {
 	int	i;
+	int	x;
 
 	i = 0;
 	while (i < len)
 	{
-		ft_printf("declare -x %s\n", envp[sorted_indexes[i]]);
+		x = 0;
+		ft_printf("declare -x ");
+		while (env[sorted_indexes[i]][x] != '=' && env[sorted_indexes[i]][x] != '\0')
+		{
+			ft_putchar_fd(env[sorted_indexes[i]][x], 1);
+			x++;
+		}
+		ft_putstr_fd("=\"", 1);
+		x++;
+		while (env[sorted_indexes[i]][x] != '\0')
+		{
+			ft_putchar_fd(env[sorted_indexes[i]][x], 1);
+			x++;
+		}
+		ft_putstr_fd("\"\n", 1);
 		i++;
 	}
 }
 
-static void	sort_and_print(char **envp, int len)
+static void	sort_and_print(char **env, int len)
 {
 	int	sorted_indexes[len];
 	int	i;
@@ -62,8 +77,8 @@ static void	sort_and_print(char **envp, int len)
 		x = i + 1;
 		while (x < len)
 		{
-			if (ft_strncmp(envp[sorted_indexes[min]], envp[sorted_indexes[x]],
-							ft_strlen(envp[sorted_indexes[min]])) > 0)
+			if (ft_strncmp(env[sorted_indexes[min]], env[sorted_indexes[x]],
+							ft_strlen(env[sorted_indexes[min]])) > 0)
 				min = x;
 			x++;
 		}
@@ -72,13 +87,13 @@ static void	sort_and_print(char **envp, int len)
 		sorted_indexes[i] = temp;
 		i++;
 	}
-	print_sorted(envp, sorted_indexes, len);
+	print_sorted(env, sorted_indexes, len);
 }
 
-void	builtin_export_no_arguments(char **envp)
+void	builtin_export_no_arguments(char **env)
 {
 	int	len;
 
-	len = get_envp_len(envp);
-	sort_and_print(envp, len);
+	len = get_env_len(env);
+	sort_and_print(env, len);
 }
