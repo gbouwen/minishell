f65dc4aca0855ca723ce4ffddee2a4011a0e12ba
gbouwen <gijsbouwen@gmail.com>
2020-11-10 11:16:04 +0100

    expander proto

--- executer/executer.c
+++ executer/executer.c
@@ -6,7 +6,7 @@
 /*   By: gbouwen <gbouwen@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/03 15:28:53 by gbouwen       #+#    #+#                 */
-/*   Updated: 2020/11/09 13:51:58 by gbouwen       ########   odam.nl         */
+/*   Updated: 2020/11/10 11:15:48 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ int	executer(t_data *data)
 		return (-1);
 	lexer(data);
 	data->tree = parser(&data->lexer);
+	//expander(data);
 	if (data->tree != NULL)
 	{
 		command_loop(data);
