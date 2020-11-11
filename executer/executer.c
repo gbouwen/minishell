2dabe0322197a9dc42e603d13454eb326c0e18be
tiemen <tiemenr@gmail.com>
2020-11-11 12:09:37 +0100

    merge

--- executer/executer.c
+++ executer/executer.c
@@ -6,7 +6,7 @@
 /*   By: gbouwen <gbouwen@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/03 15:28:53 by gbouwen       #+#    #+#                 */
-/*   Updated: 2020/11/10 15:59:03 by tiemen        ########   odam.nl         */
+/*   Updated: 2020/11/11 12:07:19 by tiemen        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,12 @@ int	executer(t_data *data)
 		return (-1);
 	lexer(data);
 	data->tree = parser(&data->lexer);
-	expander(data);
+//	expander(data);
 	print_tree(data->tree); //
-	if (data->tree != NULL && data->expand_error == 0)
-		command_loop(data);
-	delete_tree(data->tree);
-	free(data->cmdline);
-	ft_lstclear(&data->lexer.token_list, free_list_content);
+	// if (data->tree != NULL && data->expand_error == 0)
+	// 	command_loop(data);
+	// delete_tree(data->tree);
+	// free(data->cmdline);
+	// ft_lstclear(&data->lexer.token_list, free_list_content);
 	return (1);
 }
