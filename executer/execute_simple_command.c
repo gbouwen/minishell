ccd1c683afa7e68a63e4f28561a5372cc4d4c5aa
Gijs Bouwen <gijsbouwen@gmail.com>
2020-11-12 15:01:17 +0100

    changed ft_strncmp to take ft_strlen(node->content)

--- executer/execute_simple_command.c
+++ executer/execute_simple_command.c
@@ -6,7 +6,7 @@
 /*   By: gbouwen <gbouwen@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/11 14:07:33 by gbouwen       #+#    #+#                 */
-/*   Updated: 2020/11/12 14:10:11 by gbouwen       ########   odam.nl         */
+/*   Updated: 2020/11/12 15:00:47 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 
 int	check_builtin(t_node *node, t_data *data)
 {
-	if (ft_strncmp(node->content, "echo", 4) == 0)
+	if (ft_strncmp(node->content, "echo", ft_strlen(node->content)) == 0)
 		builtin_echo(data->tree);
-	else if (ft_strncmp(node->content, "pwd", 3) == 0)
+	else if (ft_strncmp(node->content, "pwd", ft_strlen(node->content)) == 0)
 		builtin_pwd(data->tree);
-	else if (ft_strncmp(node->content, "cd", 2) == 0)
+	else if (ft_strncmp(node->content, "cd", ft_strlen(node->content)) == 0)
 		builtin_cd(data->tree, data->env_variables);
-	else if (ft_strncmp(node->content, "export", 6) == 0)
+	else if (ft_strncmp(node->content, "export", ft_strlen(node->content)) == 0)
 		builtin_export(data);
-	else if (ft_strncmp(node->content, "unset", 5) == 0)
+	else if (ft_strncmp(node->content, "unset", ft_strlen(node->content)) == 0)
 		builtin_unset(data);
-	else if (ft_strncmp(node->content, "env", 3) == 0)
+	else if (ft_strncmp(node->content, "env", ft_strlen(node->content)) == 0)
 		builtin_env(data->tree, data->env_variables);
-	else if (ft_strncmp(node->content, "exit", 4) == 0)
+	else if (ft_strncmp(node->content, "exit", ft_strlen(node->content)) == 0)
 		builtin_exit(data);
 	else
 		return (0);
