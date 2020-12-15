87ead2576f313277bf504537ae75945c81b609a8
gbouwen <gijsbouwen@gmail.com>
2020-12-15 14:49:07 +0100

    multiple redirections babyyyyyyyyyyy

--- executer/redirect_out.c
+++ executer/redirect_out.c
@@ -6,7 +6,7 @@
 /*   By: gbouwen <gbouwen@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/12/15 14:09:17 by gbouwen       #+#    #+#                 */
-/*   Updated: 2020/12/15 14:43:06 by gbouwen       ########   odam.nl         */
+/*   Updated: 2020/12/15 14:48:13 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,12 @@ void	redirect_out(t_data *data, t_node *node)
 	int	new_fd;
 
 	save_out = dup(STDOUT_FILENO);
+	while (node->left!= NULL)
+		node = node->left;
 	new_fd = open(node->content, O_RDWR);
 	new_fd = dup2(new_fd, STDOUT_FILENO);
 	if (new_fd != -1)
-		execute_simple_command(data, node->right);
+		execute_simple_command(data, data->tree->right);
 	close(STDOUT_FILENO);
 	close(new_fd);
 	dup2(save_out, STDOUT_FILENO);
