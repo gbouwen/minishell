db91b6736ee89cf6f9c8033ed4b1bf568944f88f
gbouwen <gijsbouwen@gmail.com>
2020-12-18 14:32:03 +0100

    added check if node exists to prevent segfault

--- executer/command_loop.c
+++ executer/command_loop.c
@@ -6,7 +6,7 @@
 /*   By: gbouwen <gbouwen@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/03 16:19:03 by gbouwen       #+#    #+#                 */
-/*   Updated: 2020/12/17 14:34:26 by gbouwen       ########   odam.nl         */
+/*   Updated: 2020/12/18 14:30:40 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,14 +29,15 @@ void	command_loop(t_data *data)
 	node = data->tree;
 	while (node != NULL)
 	{
-		save_out = dup(STDOUT_FILENO);
 		save_in = dup(STDIN_FILENO);
+		save_out = dup(STDOUT_FILENO);
 		if (node->type == NODE_SEQUENCE)
 		{
 			if (node->right->type == FILE_OUT || node->right->type == FILE_OUT_APPEND || node->right->type == FILE_IN)
 			{
 				redirections_loop(node->right);
-				execute_simple_command(data, node->right->right);
+				if (node->right->right)
+					execute_simple_command(data, node->right->right);
 				restore_stdin_stdout(save_in, save_out);
 			}
 			else
@@ -47,7 +48,8 @@ void	command_loop(t_data *data)
 		if (node->type == FILE_OUT || node->type == FILE_OUT_APPEND || node->type == FILE_IN)
 		{
 			redirections_loop(node);
-			execute_simple_command(data, node->right);
+			if (node->right)
+				execute_simple_command(data, node->right);
 			restore_stdin_stdout(save_in, save_out);
 		}
 		node = node->left;
