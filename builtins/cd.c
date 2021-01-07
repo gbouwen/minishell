6adb94987a4fb8f02693556c7e131d5a7adc3838
gbouwen <gijsbouwen@gmail.com>
2021-01-07 12:30:31 +0100

    improved cd

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: gbouwen <gbouwen@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/02 15:52:00 by gbouwen       #+#    #+#                 */
-/*   Updated: 2021/01/06 14:16:05 by gbouwen       ########   odam.nl         */
+/*   Updated: 2021/01/07 12:27:35 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,48 @@ static void	change_pwd_env(char **envp)
 	}
 }
 
+static int	check_home(char **envp)
+{
+	int	i;
+
+	i = 0;
+	while (envp[i] != NULL)
+	{
+		if (ft_strncmp(envp[i], "HOME=", 5) == 0)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+static char	*get_home_var(char **envp)
+{
+	int	i;
+
+	i = 0;
+	while (envp[i] != NULL)
+	{
+		if (ft_strncmp(envp[i], "HOME=", 5) == 0)
+			return (envp[i] + 5);
+		i++;
+	}
+	return (NULL);
+}
+
+static void	correct_cd_error_message(t_node *temp)
+{
+	ft_printf("bash: cd: ");
+	if (temp->right == NULL)
+		ft_printf("%s: No such file or directory\n", temp->content);
+	else
+		ft_printf("too many arguments\n");
+}
+
 void	builtin_cd(t_node *node, char **envp)
 {
 	t_node	*temp;
 	int		ret;
+	char	*home;
 
 	temp = NULL;
 	ret = -1;
@@ -42,8 +80,16 @@ void	builtin_cd(t_node *node, char **envp)
 		temp = node->right;
 	if (temp != NULL)
 		ret = chdir(temp->content);
-	if (ret == -1)
-		ft_printf("error: could not change directory\n");
+	if (ret == -1 && node->right == NULL && check_home(envp) == 1)
+	{
+		home = get_home_var(envp);
+		if (home != NULL)
+			ret = chdir(home);
+	}
+	if (ret == -1 && node->right == NULL && check_home(envp) == 0)
+		ft_printf("bash: cd: HOME not set\n");
+	else if (ret == -1)
+		correct_cd_error_message(temp);
 	else
 		change_pwd_env(envp);
 }
