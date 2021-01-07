8a224bc0da04ed36bb34b858fe44d4bca3a69254
gbouwen <gijsbouwen@gmail.com>
2021-01-07 12:47:54 +0100

    free data

--- builtins/export_variable.c
+++ builtins/export_variable.c
@@ -6,13 +6,13 @@
 /*   By: gbouwen <marvin@codam.nl>                    +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/05 15:47:40 by gbouwen       #+#    #+#                 */
-/*   Updated: 2021/01/06 15:06:47 by gbouwen       ########   odam.nl         */
+/*   Updated: 2021/01/07 12:47:14 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 
-static char	**add_variable(char **envp, char *var)
+static char	**add_variable(t_data *data, char **envp, char *var)
 {
 	int		len;
 	char	**new_envp;
@@ -21,7 +21,7 @@ static char	**add_variable(char **envp, char *var)
 	len = get_str_array_len(envp);
 	new_envp = ft_calloc(len + 2, sizeof(char *));
 	if (!new_envp)
-		exit_error("Malloc failed");
+		free_struct_error(data, "Malloc failed");
 	i = 0;
 	while (i < len)
 	{
@@ -41,7 +41,7 @@ void		builtin_export_variable(t_data *data)
 	temp = data->tree->right;
 	while (temp)
 	{
-		data->env_variables = add_variable(data->env_variables, temp->content);
+		data->env_variables = add_variable(data, data->env_variables, temp->content);
 		temp = temp->right;
 	}
 }
