288357b6c0776653600d494fb9efe7aebaaba342
gbouwen <gijsbouwen@gmail.com>
2021-01-13 15:12:28 +0100

    fixed || echo hoi ; ls -la > test ||

--- executer/executer.c
+++ executer/executer.c
@@ -6,7 +6,7 @@
 /*   By: gbouwen <gbouwen@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/03 15:28:53 by gbouwen       #+#    #+#                 */
-/*   Updated: 2021/01/12 16:26:58 by gbouwen       ########   odam.nl         */
+/*   Updated: 2021/01/13 11:22:45 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,11 @@ int	executer(t_data *data)
 		return (-1);
 	lexer(data);
 	if (data->lexer.state != GENERAL || data->lexer.error == 1)
+	{
+		free(data->cmdline);
+		ft_lstclear(&data->lexer.token_list, free_list_content);
 		return (1);
+	}
 	expand_variables(data);
 	data->tree = parser(&data->lexer);
 	expand_files(data, data->tree);
