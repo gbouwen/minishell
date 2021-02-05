c0c48ca6277557243b605fef8fa5762761b4a3f1
gbouwen <gijsbouwen@gmail.com>
2021-02-05 16:33:40 +0100

    header

--- error/signals.c
+++ error/signals.c
@@ -6,7 +6,7 @@
 /*   By: tblanker <tblanker@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/12 13:49:45 by tblanker      #+#    #+#                 */
-/*   Updated: 2021/01/28 14:34:53 by gbouwen       ########   odam.nl         */
+/*   Updated: 2021/02/05 15:42:53 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
