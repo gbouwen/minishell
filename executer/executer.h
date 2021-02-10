e6429385b7608e14950bc5062a1ef22b7524b5a4
gbouwen <gijsbouwen@gmail.com>
2021-02-10 13:02:20 +0100

    merge

--- executer/executer.h
+++ executer/executer.h
@@ -6,11 +6,7 @@
 /*   By: gbouwen <gbouwen@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/03 15:29:10 by gbouwen       #+#    #+#                 */
-<<<<<<< HEAD
-/*   Updated: 2021/02/09 15:58:09 by gbouwen       ########   odam.nl         */
-=======
-/*   Updated: 2021/02/09 15:19:54 by tiemen        ########   odam.nl         */
->>>>>>> e250f8fc52b9d716bc78ee1b1449585870897d85
+/*   Updated: 2021/02/10 13:02:07 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,15 +37,8 @@ void			execute_pipe(t_data *data, t_node *node);
 void			execute_simple_command(t_data *data, t_node *node);
 void			fork_and_execute(t_data *data, t_node *node);
 
-<<<<<<< HEAD
 void			redirections_loop(t_data *data, t_node *node, int *current_fds);
 void			set_redirections(t_data *data, t_node *node, int *current_fds);
-=======
-void	child_actions(t_data *data, t_node *node);
-
-int				redirections_loop(t_data *data, t_node *node);
-int				set_redirections(t_data *data, t_node *node, int current_fd);
->>>>>>> e250f8fc52b9d716bc78ee1b1449585870897d85
 
 char			*find_path_variable(char **env);
 
