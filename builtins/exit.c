7869aa0dc22afdf489c50800bd6bab68ad673d06
gbouwen <gijsbouwen@gmail.com>
2021-02-17 12:54:12 +0100

    write error messages to stderr in exit

--- builtins/exit.c
+++ builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: gbouwen <gbouwen@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/02 17:03:49 by gbouwen       #+#    #+#                 */
-/*   Updated: 2021/02/17 11:59:46 by gbouwen       ########   odam.nl         */
+/*   Updated: 2021/02/17 12:53:40 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,41 +26,46 @@ static int	check_if_number(char *str)
 	return (0);
 }
 
-static void	correct_exit(t_data *data, t_node *node)
+static void	exit_with_numeric_arguments(t_data *data, t_node *node)
 {
 	int	exit_status;
 
+	exit_status = 0;
+	if (node->right != NULL)
+	{
+		ft_putstr_fd("exit\n", 2);
+		ft_putstr_fd("minishell: exit: too many arguments\n", 2);
+		g_question_mark = 1;
+	}
+	else
+	{
+		ft_putstr_fd("exit\n", 2);
+		exit_status = ft_atoi(node->content);
+		free_struct(data);
+		exit(exit_status);
+	}
+}
+
+static void	correct_exit(t_data *data, t_node *node)
+{
 	if (check_if_number(node->content) == 0)
 	{
-		ft_printf("exit\n");
-		ft_printf("minishell: exit:");
-		ft_printf(" %s: numeric argument required\n", node->content);
+		ft_putstr_fd("exit\n", 2);
+		ft_putstr_fd("minishell: exit: ", 2);
+		ft_putstr_fd(node->content, 2);
+		ft_putstr_fd(": numeric argument required\n", 2);
 		free_struct(data);
 		exit(2);
 	}
 	else
-	{
-		if (node->right != NULL)
-		{
-			ft_printf("exit\n");
-			ft_printf("minishell: exit: too many arguments\n");
-			g_question_mark = 1;
-		}
-		else
-		{
-			ft_printf("exit\n");
-			exit_status = ft_atoi(node->content);
-			free_struct(data);
-			exit(exit_status);
-		}
-	}
+		exit_with_numeric_arguments(data, node);
 }
 
 void		builtin_exit(t_data *data, t_node *node)
 {
 	if (node->right == NULL)
 	{
-		ft_printf("exit\n");
+		ft_putstr_fd("exit\n", 2);
 		free_struct(data);
 		exit(0);
 	}
